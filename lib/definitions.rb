c1426e36deb180184f93f61efad390fb0b2e12eb
JuliannaGreen <juliannaggreen@gmail.com>
2015-08-14 11:09:34 -0700

    add an .all method which will create an empty array for all  definitions

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -1,9 +1,14 @@
 class Definitions
 
   attr_reader :definition
+  @@all_definitions = []
 
   define_method(:initialize) do |attributes|
     @definition = attributes.fetch(:definition)
+  end
+
+  define_singleton_method(:all) do
+    @@all_definitions
   end
 
 end
