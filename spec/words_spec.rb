94e026eb88e85f4f49c7e69793810241bc98fc24
JuliannaGreen <juliannaggreen@gmail.com>
2015-08-14 10:14:41 -0700

    add second spec .all which creates an empty array for words/added hash for Words.new initialization

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -5,8 +5,15 @@ describe(Words) do
 
   describe("#word") do
     it("lets you make a new word") do
-      new_word = Words.new("Frowzy")
+      new_word = Words.new({:word=> "Frowzy", :definition =>" scruffy and neglected in appearance"})
       expect(new_word.word()).to(eq("Frowzy"))
     end
   end
+
+  describe('.all') do
+  it('creates empty array to store words') do
+    expect(Words.all()).to(eq([]))
+  end
+end
+
 end
