161ec469f806951a23322a0de348c18eedd3a9df
JuliannaGreen <juliannaggreen@gmail.com>
2015-08-14 10:16:33 -0700

    add method .all which creates empty array for words

--- lib/words.rb
+++ lib/words.rb
@@ -1,8 +1,14 @@
 class Words
 
+  @@all = []
+
   attr_reader :word
 
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
+  end
+
+  define_singleton_method(:all) do
+  @@all
   end
 end
