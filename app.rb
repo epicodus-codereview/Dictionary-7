80d4eb3ac485feff6c457ac9673f2aa54d382623
JuliannaGreen <juliannaggreen@gmail.com>
2015-08-14 13:39:34 -0700

    add a bunch of pathways in app.rb that have yet to work.

--- app.rb
+++ app.rb
@@ -10,10 +10,24 @@ get('/') do
 end
 
 post('/new_word') do
-  @all = Words.all()
+  @words = Words.all()
   word = params.fetch('word')
   new_word = Words.new(:word =>word)
   new_word.save()
   @words = Words.all()
   erb(:index)
+end
+
+post('/add_definition/:id') do
+  @word = Words.find(params.fetch('id'))
+  @definitions = @word.definitions()
+  @definitions = Definitions.all()
+  definition = params.fetch('definition')
+  new_definition = Definitions.new(:definition =>definition)
+  new_definition.save()
+  @word.add_definition(new_definition)
+  @definitions = Definitions.all()
+  erb(:add_definitions)
+
+
 end
